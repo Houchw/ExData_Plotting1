f8b7719c39868f7738fd0b11cab2ea431ccf48ac
Houchw <houchw@163.com>
2015-04-11 00:11:25 +0800

    New Changes
    Some mistakes has been fixed.

--- figure/plot4.R
+++ figure/plot4.R
@@ -15,7 +15,7 @@ png("plot4.png")
 par(mfrow = c(2, 2))
 plot(x = mydata$Time, y = mydata[, 3], type = "l", 
      xlab = "", ylab = "Global Active Power (kilowatts)")
-plot(x = mydata$Time, y = mydata[, 4], type = "l", 
+plot(x = mydata$Time, y = mydata[, 5], type = "l", 
      xlab = "datetime", ylab = "Voltage")
 plot(x = mydata$Time, y = mydata[, 7], type = "l", 
      xlab = "", ylab = "Energy sub metering")
@@ -23,6 +23,6 @@ lines(x = mydata$Time, y = mydata[, 8], type = "l", col = "red")
 lines(x = mydata$Time, y = mydata[, 9], type = "l", col = "blue")
 legend("topright", legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), 
        lty = c(1, 1, 1), col = c("black", "red", "blue"))
-plot(x = mydata$Time, y = mydata[, 3], type = "l", 
+plot(x = mydata$Time, y = mydata[, 4], type = "l", 
      xlab = "datetime", ylab = "Global_reactive_power")
 dev.off()
